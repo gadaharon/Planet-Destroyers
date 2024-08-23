dab9a5c0d3eb2e25cf8be038d1c23a280dccb48a
Gad Aharon <gadaharon91@gmail.com>
2024-08-23 20:06:50 +0300

    Add custom sprite with animation to enemy

--- Assets/Scripts/Enemy/EnemyController/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController/EnemyController.cs
@@ -6,11 +6,13 @@ public class EnemyController : MonoBehaviour, IDamagable
     public static Action OnEnemyDeath;
 
     [SerializeField] bool lookAtPlayer = true;
+    [SerializeField] Transform sprite;
 
     Transform player;
 
     // Knockback knockback;
     EnemyBase enemyBase;
+    bool isFacingRight = false;
 
     void Awake()
     {
@@ -29,6 +31,7 @@ public class EnemyController : MonoBehaviour, IDamagable
         if (lookAtPlayer)
         {
             LookAtPlayer();
+            FlipSprite();
         }
 
         if (enemyBase.isRangeAttackEnemy)
@@ -43,6 +46,24 @@ public class EnemyController : MonoBehaviour, IDamagable
         Vector3 direction = player.position - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, angle + angleOffset);
+    }
+
+
+
+    void FlipSprite()
+    {
+        if (sprite == null) { return; }
+
+        if (transform.position.x < player.position.x)
+        {
+            isFacingRight = true;
+            sprite.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (transform.position.x > player.position.x)
+        {
+            isFacingRight = false;
+            sprite.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     public void TakeDamage(string damageDealerTag, int damage)
